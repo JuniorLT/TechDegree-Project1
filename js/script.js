211b88071103471b4beb418976e92b413232c5fa
JuniorLT <53494833+JuniorLT@users.noreply.github.com>
2019-08-02 13:51:28 -0700

    added print quote function, fixed syntax errors, made comments

--- js/script.js
+++ js/script.js
@@ -1,78 +1,73 @@
+/** Junior Lam Tiang : Project 1 **/
+/** Random Quote Generator **/
+/**Treehouse FSJS Techdegree: **/
 
-Treehouse FSJS Techdegree:
-
+/* array that holds 5 quotes */
 let quotes = [
 {
-	quote: "I have the high ground."
-	source: "Obi Wan Kenobi"
-	citation: "Star Wars Episode III (Revenge of the Sith)"
+	quote: "I have the high ground.",
+	source: "Obi Wan Kenobi",
+	citation: "Star Wars Episode III (Revenge of the Sith)",
 	year: "2005"
 },
 {
-	quote: "Get over here!"
-	source: "Scorpion"
-	citation: "Mortal Kombat"
+	quote: "Get over here!",
+	source: "Scorpion",
+	citation: "Mortal Kombat",
 	year: ""
 },
 {
-	quote: "Grass grows, birds fly, sun shines, and brother, I hurt people."
-	source: "The Scout"
-	citation: "Team Fortress 2"
+	quote: "Grass grows, birds fly, sun shines, and brother, I hurt people.",
+	source: "The Scout",
+	citation: "Team Fortress 2",
 	year: ""
 },
 {
-	quote: "It’s not that I’m so smart, it’s just that I stay with problems longer."
-	source: "Albert Einstein"
-	citation: ""
+	quote: "It is not that I am so smart, it is just that I stay with problems longer.",
+	source: "Albert Einstein",
+	citation: "",
 	year: ""
 },
 {
-	quote: "If you spend too much time thinking about a thing, you’ll never get it done."
-	source: "Bruce Lee"
-	citation: ""
+	quote: "If you spend too much time thinking about a thing, you will never get it done.",
+	source: "Bruce Lee",
+	citation: "",
 	year: ""
 }
 ];
 
 
+/* function that gets a random quote from the array */
 function getRandomQuote() {
-	var randomNumber = Math.floor(Math.random() * 6);		/* creates a random number between 0 and 5 */
+	let randomNumber = Math.floor(Math.random() * 6);		/* variable set to create a random number between 0 and 5 */
 	return quotes[randomNumber]		/*returns the object from the array according to the index position of the random number created above */
 }
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
+/* function that will call function above (getting the quote) and will print it */
 function printQuote() {
-	var randomQuote = getRandomQuote();
-	var box = document.getElementById("quote-box");
-	var text = ''
-	var markup = <p class="quote"> [randomQuote.quote] </p>
-	<p class="source"> [randomQuote.source]
-	<span class="citation"> [randomQuote.citation] </span>
-	<span class="year"> [randomQuote.year] </span></p>;
-	box = markup;
+	let randomQuote = getRandomQuote();		/* variable set to call the function when this function is called */
+	let box = document.getElementById("quote-box");		/* variable set to get html element that will present the quote once concatenated */
+	let text = '';		/* variable set to an empty string */
+	text += '<p class="quote">' + randomQuote.quote + '</p>';	/* gets the quote property from the object and concatenates it to the string */
+	text += '<p class="source">' + randomQuote.source;	/* gets the source property from the object and concatenates it to the string */
+
+	/* conditional that checks if there is or isn't a citation property */
+	if (randomQuote.citation === ""){
+		text += '';
+	} else{
+		text += '<span class="citation">' + randomQuote.citation + '</span>';
+	}
+
+	/* conditional that checks if there is or isn't a year property */
+	if (randomQuote.year === ""){
+		text += '';
+	} else{
+		text += '<span class="year">' + randomQuote.year + '</span></p>';
+	}
+	
+	/* sets the html element equal to the quote object and it's properties */
+	box.innerHTML = text;
 }
 
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote(), false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+/* listens for click then prints another quote */
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
